f5bf85e8d7d2b2f5ec9e7782f6757d2c8dcb8cec
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-16 14:00:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-const cards = document.querySelectorAll(".card");
+window.addEventListener("load", () => {
+  const cards = document.querySelectorAll(".card");
 
-window.addEventListener("scroll", () => {
   cards.forEach(card => {
-    const top = card.getBoundingClientRect().top;
-    const trigger = window.innerHeight - 50; // pehle se jaldi show hoga
-
-    if (top < trigger) {
-      card.style.opacity = "1";
-      card.style.transform = "translateY(0)";
-    }
+    card.style.opacity = "1";
+    card.style.transform = "translateY(0)";
   });
 });
