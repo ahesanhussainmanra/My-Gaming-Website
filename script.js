112c702a68851fde1a4198707094826e7643b623
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-16 09:41:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cards = document.querySelectorAll(".card");
 window.addEventListener("scroll", () => {
   cards.forEach(card => {
     const top = card.getBoundingClientRect().top;
-    const trigger = window.innerHeight - 100;
+    const trigger = window.innerHeight - 50; // pehle se jaldi show hoga
 
     if (top < trigger) {
       card.style.opacity = "1";
