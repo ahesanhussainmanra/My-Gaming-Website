67bfd53b909b057feb4ff0b27a2bdf6464709f77
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-17 11:34:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-window.addEventListener("load", () => {
-  const cards = document.querySelectorAll(".card");
+const elements = document.querySelectorAll(".card");
 
-  cards.forEach(card => {
-    card.style.opacity = "1";
-    card.style.transform = "translateY(0)";
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
   });
+});
+
+elements.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(50px)";
+  observer.observe(el);
 });
