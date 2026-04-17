e19a3fe51c69839b672e9061f2510eed76fcb7ec
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-17 11:46:04 +0530

    Add files via upload

---
+++ particles-config.js
@@ -0,0 +1,37 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 80
+    },
+    color: {
+      value: "#00ffff"
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 3
+    },
+    move: {
+      speed: 2
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#00ffff",
+      opacity: 0.4,
+      width: 1
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      }
+    }
+  }
+});
