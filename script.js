d0b25cb2dcdbf8eee9e0b2c65d7f0672c8fcad58
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-18 16:48:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const elements = document.querySelectorAll(".card");
+const faders = document.querySelectorAll(".fade");
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
+window.addEventListener("scroll", () => {
+  faders.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      el.classList.add("show");
     }
   });
 });
+window.onload = () => {
+  document.getElementById("loader").style.display = "none";
+};
+const glow = document.createElement("div");
+glow.style.position = "fixed";
+glow.style.width = "20px";
+glow.style.height = "20px";
+glow.style.background = "cyan";
+glow.style.borderRadius = "50%";
+glow.style.pointerEvents = "none";
+glow.style.filter = "blur(10px)";
+glow.style.zIndex = "999";
+document.body.appendChild(glow);
 
-elements.forEach(el => {
-  el.style.opacity = 0;
-  el.style.transform = "translateY(50px)";
-  observer.observe(el);
+document.addEventListener("mousemove", e => {
+  glow.style.left = e.clientX + "px";
+  glow.style.top = e.clientY + "px";
 });
