d2b74037765435cbb0b258708bb1517dd830ffaf
ahesanhussainmanra <akbaralimanra141@gmail.com>
2026-04-19 20:35:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,21 +14,6 @@ elements.forEach(el => {
   el.style.transform = "translateY(50px)";
   observer.observe(el);
 });
-const glow = document.createElement("div");
-glow.style.position = "fixed";
-glow.style.width = "20px";
-glow.style.height = "20px";
-glow.style.background = "cyan";
-glow.style.borderRadius = "50%";
-glow.style.pointerEvents = "none";
-glow.style.filter = "blur(10px)";
-glow.style.zIndex = "999";
-document.body.appendChild(glow);
-
-document.addEventListener("mousemove", e => {
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
-});
 window.onload = () => {
   document.getElementById("loader").style.display = "none";
 };
